9f6a925ec0e6d4eb4e8f9164d7cb779af304680b
Andrew Goodricke <github@andrewgoodricke.com>
2016-08-22 15:18:27 -0300

    Builder: Upgraded

--- build.js
+++ build.js
@@ -1,13 +1,42 @@
 'use strict';
-
-// var debug_lib   = require('debug');
-// var debug       = debug_lib('nuxeo-build');
-// var error       = debug_lib('nuxeo-build:error');
-var path        = require('path');
-var yaml_config = require('node-yaml-config');
-var builder     = require('nuxeo-docs-builder');
+/* eslint-env es6 */
 
 
-var config = yaml_config.load(path.join(__dirname, '/config.yml'));
+// Set Debugging up
+if (!process.env.DEBUG) {
+    process.env.DEBUG = '*:info,*:error';
+}
 
-builder(config, path.join(__dirname, '/src'), path.join(__dirname, '/site'));
+const debug_lib = require('debug');
+// const debug = debug_lib('nuxeo-build');
+const error = debug_lib('nuxeo-build:error');
+const path = require('path');
+const co = require('co');
+const extend = require('lodash.defaultsdeep');
+const builder_lib = require('nuxeo-docs-builder');
+const pre_builder = builder_lib.pre_builder;
+const builder = builder_lib.builder;
+
+// Working copy
+const target_repo_path = path.join(__dirname, 'src');
+const target_repo_site = path.join(__dirname, 'site');
+
+error('target_repo_src: %s', target_repo_path);
+co(function *() {
+    // Pre-build
+    const pre_build = [
+        pre_builder(target_repo_path)
+    ];
+    const metadata = {};
+    const pre_build_result = yield pre_build;
+    pre_build_result.forEach(function (data) {
+        extend(metadata, data);
+    });
+
+    // Build
+    yield builder(target_repo_path, metadata, target_repo_site);
+})
+.catch(err => {
+    error(err);
+    throw err;
+});
