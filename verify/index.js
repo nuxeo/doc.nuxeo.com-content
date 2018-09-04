930764a5e9ab0de217c8fb0756db94a36bcff804
Andrew Goodricke <github@andrewgoodricke.com>
2018-09-04 10:10:17 +0100

    Upgraded to v10.3.0 of docs builder

--- verify/index.js
+++ verify/index.js
@@ -1,14 +1,13 @@
 'use strict';
 /* eslint-env es6 */
 
-const test = require('tape');
+const { test } = require('tap');
 const fs = require('fs');
 const path = require('path');
 
 const site_path = path.join(__dirname, '../site');
 
-test('lib assets directory should be generated', (assert) => {
-
+test('lib assets directory should be generated', assert => {
     assert.doesNotThrow(() => fs.accessSync(site_path, fs.F_OK), void 0, `${site_path} directory is present`);
     assert.doesNotThrow(() => fs.accessSync(path.join(site_path, 'assets'), fs.F_OK), void 0, 'assets directory is present');
     assert.doesNotThrow(() => fs.accessSync(path.join(site_path, 'idedoc'), fs.F_OK), void 0, 'nxdoc space directory is present');
