6458242290bd72e1f2dbfc245d71d5a400b1fbf9
Andrew Goodricke <github@andrewgoodricke.com>
2019-01-21 13:29:15 +0000

    Search page added for Algolia

--- verify/index.js
+++ verify/index.js
@@ -8,11 +8,19 @@ const path = require('path');
 const site_path = path.join(__dirname, '../site');
 
 test('lib assets directory should be generated', assert => {
-    assert.doesNotThrow(() => fs.accessSync(site_path, fs.F_OK), void 0, `${site_path} directory is present`);
-    assert.doesNotThrow(() => fs.accessSync(path.join(site_path, 'assets'), fs.F_OK), void 0, 'assets directory is present');
-    assert.doesNotThrow(() => fs.accessSync(path.join(site_path, 'idedoc'), fs.F_OK), void 0, 'nxdoc space directory is present');
+  assert.doesNotThrow(() => fs.accessSync(site_path, fs.F_OK), void 0, `${site_path} directory is present`);
+  assert.doesNotThrow(
+    () => fs.accessSync(path.join(site_path, 'assets'), fs.F_OK),
+    void 0,
+    'assets directory is present'
+  );
+  assert.doesNotThrow(
+    () => fs.accessSync(path.join(site_path, 'idedoc'), fs.F_OK),
+    void 0,
+    'nxdoc space directory is present'
+  );
 
-    assert.end();
+  assert.end();
 });
 
 // test('site should have key files', function (assert) {
